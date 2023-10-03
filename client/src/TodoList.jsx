cf84a4056d7119049e7bf7c9ff1455e735c2683a
Carrara1 <joao.queirozcarrara@gmail.com>
2023-10-02 21:59:57 -0300

    Finished a few things in the frontend, need bugfix

--- client/src/TodoList.jsx
+++ client/src/TodoList.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
 import axios from "axios";
 import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
 
 let endpoint = "http://localhost:9000";
+const root = createRoot(document.getElementById("root"));
 
 class TodoList extends Component {
 	constructor(props) {
@@ -12,7 +14,9 @@ class TodoList extends Component {
 			task: "",
 			items: [],
 		};
+		this.onSubmit = this.onSubmit.bind(this);
 	}
+
 	componentDidMount() {
 		this.getTask();
 	}
@@ -23,7 +27,31 @@ class TodoList extends Component {
 		});
 	};
 
-	onSubmit;
+	onSubmit = () => {
+		let { task } = this.state;
+
+		if (task) {
+			axios
+				.post(
+					endpoint + "/api/task",
+					{
+						task,
+					},
+					{
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded",
+						},
+					}
+				)
+				.then((res) => {
+					this.getTask();
+					this.setState({
+						task: "",
+					});
+					console.log(res);
+				});
+		}
+	};
 
 	getTask = () => {
 		axios.get(endpoint + "/api/task").then((res) => {
@@ -68,65 +96,64 @@ class TodoList extends Component {
 	};
 
 	updateTask = (id) => {
-		axios.put(endpoint + "/api/task/" + id, {
-			headers:{
-				"Content-Type": "application/x-www-form-urlencoded",
-			}
-		}).then((res) => {
-			console.log(res);
-			this.getTask();
-		})
-
+		axios
+			.put(endpoint + "/api/task/" + id, {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				this.getTask();
+			});
+	};
 	undoTask = (id) => {
-		axios.put(endpoint + "/api/task/" + id, {
-			headers:{
-				"Content-Type": "application/x-www-form-urlencoded",
-			}
-		}).then((res) => {
-			console.log(res);
-			this.getTask();
-		})
-	}
-		
+		axios
+			.put(endpoint + "/api/task/" + id, {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				this.getTask();
+			});
+	};
+
 	deleteTask = (id) => {
-		axios.delete(endpoint + "/api/task/" + id, {
-			headers:{
-				"Content-Type": "application/x-www-form-urlencoded",
-			}
-		}).then((res) => {
-			console.log(res);
-			this.getTask();
-		})
-	}
-
-	render() {
-
-		return (
-			<div>
-				<div className="row">
-					<Header className="header" as="h2" color="yellow">
-						To do List
-					</Header>
-				</div>
-				<div ClassName="row">
-					<Form onSubmit={this.onSubmit}>
-						<Input
-							type="text"
-							name="task"
-							onChange={this.onChange}
-							value={this.state.task}
-							fluid
-							placeholder="Create Task"
-						></Input>
-						{/*<Button>Create Task</Button> */}
-					</Form>
-				</div>
-				<div className="row">
-					<Card.Group>{this.state.items}</Card.Group>
-				</div>
-			</div>
-		);
-	}
+		axios
+			.delete(endpoint + "/api/task/" + id, {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			})
+			.then((res) => {
+				console.log(res);
+				this.getTask();
+			});
+	};
 }
+
+root.render(
+	<div>
+		<Header className="header" as="h2" color="yellow">
+			To do List
+		</Header>
+		<div className="row">
+			<Form onSubmit={this.onSubmit}>
+				<Input
+					type="text"
+					label="Create Task"
+					onChange={this.onChange}
+					value={this.state.task}
+					placeholder="Create Task"
+				></Input>
+			</Form>
+		</div>
+		<div className="row">
+			<Card.Group items={this.state.items}></Card.Group>
+		</div>
+	</div>
+);
 
 export default TodoList;
--- server/models/models.go
+++ server/models/models.go
@@ -4,7 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type TodoList struct {
+type ToDoList struct {
 	ID     primitive.ObjectID `json: "_id,omitempty" bson: "_id,omitempty"`
 	Task   string             `json: "task", omitempty`
 	Status bool               `json: "status", omitempty`
